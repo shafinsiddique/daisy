c99f691f50cab20aa3a3d10fc8e9dd302d9b6d36
Shafin Siddique <siddiqueshafin20@gmail.com>
2023-06-30 03:12:59 -0400

    applicative style parsing.

--- daisy/_build/default/lib/combinator.ml
+++ daisy/_build/default/lib/combinator.ml
@@ -3,6 +3,9 @@ type 'a parsing_result = ParsingSuccess of 'a * string | ParsingError of string
 type 'a parser = Parser of (string -> 'a parsing_result)
 
 let run_parser parser input = let (Parser f) = parser in f input
+
+let rec create_list current length lst = if current >= length 
+    then lst else create_list (current+1) length (List.cons current lst)
 (*
 
 The simplest of parsers. Given a character, if it is the current character then return succes. 
@@ -49,6 +52,9 @@ let sequence parsers =
     in _sequence parsers []
 
 
+(* 
+A word parser. 
+*)
 let word_parser word = Parser 
   (fun input -> let parsers = String.fold_left (fun lst char -> (List.cons char lst)) [] word |> List.rev |> List.map (fun chr -> char_parser chr)  in 
     let result = run_parser (sequence parsers) input in 
@@ -56,3 +62,41 @@ let word_parser word = Parser
         ParsingSuccess (value, rest) -> ParsingSuccess (List.fold_left (fun str chr -> str ^ (String.make 1 chr)) "" value, rest)
         | ParsingError e -> ParsingError e)
 
+(*
+A parser if there's a digit, returns the digit, otherwise returns None.
+*)
+let digit_parser = any_of (List.map 
+(fun num -> let chr = match num with 
+  0 -> '0'
+  | 1 -> '1'
+  | 2 -> '2'
+  | 3 -> '3'
+  | 4 -> '4'
+  | 5 -> '5'
+  | 6 -> '6'
+  | 7 -> '7'
+  | 8 -> '8'
+  | 9 -> '9'
+  | _ -> 'h' in char_parser chr) (create_list 0 10 []))
+
+
+(*
+  
+I want a parser that takes in anything and wraps it in a parser. 
+
+handle_char <*> char_parser 'a' 
+
+runParser p1
+
+*)
+
+let fmap f parser = 
+  Parser (fun input -> match run_parser parser input with 
+                       ParsingSuccess (value, rest) -> ParsingSuccess ((f value), rest)
+                      | ParsingError e -> ParsingError e)
+
+let pure value = Parser (fun input -> ParsingSuccess (value, input))
+
+let (<*>) p1 p2 = Parser (fun input -> match run_parser p1 input with 
+                            ParsingSuccess (func, rest) -> run_parser (fmap func p2) rest
+                            | ParsingError e -> ParsingError e )
