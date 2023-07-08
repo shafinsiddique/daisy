f4a41d37118f9108f1949f844a11ce607cf6387e
Shafin Siddique <siddiqueshafin20@gmail.com>
2023-07-08 03:44:15 -0400

    more parsing rules.

--- daisy/_build/default/lib/markdown.ml
+++ daisy/_build/default/lib/markdown.ml
@@ -1,8 +1,7 @@
 open Combinator
 
-type markdown = Heading of {level: int; text: string} | MarkdownChar of char 
-  | Bold of (markdown list) | Italic of (markdown list) | Link of (markdown list * string)
-
+type markdown = Heading of {level: int; components: markdown list} | MarkdownChar of char 
+  | Bold of (markdown list) | Italic of (markdown list) | Link of {components: markdown list; url: string}
 (* 
    
 If any of parsers from parser list returns succesful then STOP. 
@@ -37,14 +36,19 @@ let chars_to_string chars = List.fold_left (fun str chr -> str ^ (String.make 1 
 let rec internal_parser () = 
   let bold_parser = get_parser (word_parser "**") (fun _ tokens _ -> Bold tokens)  in 
   let italic_parser = get_parser (word_parser "*") (fun _ tokens _ -> Italic tokens)  in 
-  let link_parser = pure (fun _ items _ _ url _ -> Link (items, (chars_to_string url))) <*> char_parser '[' <*> (parse_on_condition_lazy (char_parser ']') internal_parser) <*> char_parser ']' 
+  let link_parser = pure (fun _ components _ _ url _ -> Link {components;url=(chars_to_string url)}) <*> char_parser '[' <*> (parse_on_condition_lazy (char_parser ']') internal_parser) <*> char_parser ']' 
             <*> char_parser '(' <*> conditional_parser (fun chr -> chr != ')') <*> char_parser ')' in 
     any_of [bold_parser; italic_parser; link_parser; markdown_char_parser]
 and 
-get_parser p handler= pure handler <*> p <*> (parse_on_condition_lazy p internal_parser) <*> p 
-and
-get_parser2  p handler= pure handler <*> p <*> (parse_on_condition_lazy p internal_parser) <*> p 
+get_parser p handler= pure handler <*> p <*> (parse_on_condition_lazy (any_of [word_parser "\n"; p]) internal_parser) <*> p 
 
 let heading_parser =  
-  let handler tags text = Heading {level=(String.length tags); text=(List.fold_left (fun str chr -> str ^ (String.make 1 chr)) "" text)} in 
-  pure handler <*> any_of (List.map word_parser ["######";"#####";"####";"###";"##";"#"]) <*> conditional_parser (fun input -> input != '\n') 
+  let handler tags text = Heading {level=(String.length tags); components= text} in 
+  pure handler <*> any_of (List.map word_parser ["######";"#####";"####";"###";"##";"#"]) <*> 
+  one_or_more (internal_parser ())
+
+
+(*
+What's left? List, Paragraph (Outer level), continous parsing.   
+
+*)
--- daisy/lib/markdown.ml
+++ daisy/lib/markdown.ml
@@ -1,8 +1,7 @@
 open Combinator
 
-type markdown = Heading of {level: int; text: string} | MarkdownChar of char 
-  | Bold of (markdown list) | Italic of (markdown list) | Link of (markdown list * string)
-
+type markdown = Heading of {level: int; components: markdown list} | MarkdownChar of char 
+  | Bold of (markdown list) | Italic of (markdown list) | Link of {components: markdown list; url: string}
 (* 
    
 If any of parsers from parser list returns succesful then STOP. 
@@ -10,41 +9,30 @@ If any of parsers from parser list returns succesful then STOP.
 As long as those don't get matched, run the parser p. store output in lst, keep working. 
 *)
 
+
 let markdown_char_parser = 
   Parser (fun input -> match (run_parser (single_conditional_parser (fun chr -> chr != '\n')) input) with 
     ParsingSuccess (value, rest) -> ParsingSuccess (MarkdownChar value, rest)
     | ParsingError e -> ParsingError e)
 
-  
-(* let bold_parser = 
-  let handler _ chars _ = 
-      (List.fold_left (fun str chr -> str ^ (String.make 1 chr)) "" chars) in pure handler <*> 
-  word_parser "**" <*> parse_on_condition (word_parser "**") 
-                    (single_conditional_parser (fun chr -> chr != '\n'))  <*> word_parser "**" *)
-
-(*
-
-Internal Parser : bold, link, italacized, 
-*)
-
-(* let internal_parser = 
-  let parser p handler = 
-    pure handler <*> p <*> (parse_on_condition p internal_parser) <*> p  
-  and 
-    let bold_parser = parser (word_parser "##") (fun _ _ _ -> ()) in  *)
 let chars_to_string chars = List.fold_left (fun str chr -> str ^ (String.make 1 chr)) "" chars
 
 let rec internal_parser () = 
   let bold_parser = get_parser (word_parser "**") (fun _ tokens _ -> Bold tokens)  in 
   let italic_parser = get_parser (word_parser "*") (fun _ tokens _ -> Italic tokens)  in 
-  let link_parser = pure (fun _ items _ _ url _ -> Link (items, (chars_to_string url))) <*> char_parser '[' <*> (parse_on_condition_lazy (char_parser ']') internal_parser) <*> char_parser ']' 
-            <*> char_parser '(' <*> conditional_parser (fun chr -> chr != ')') <*> char_parser ')' in 
+  let link_parser = pure (fun _ components _ _ url _ -> Link {components;url=(chars_to_string url)}) <*> char_parser '[' <*> (parse_on_condition_lazy (any_of [char_parser '\n'; char_parser ']']) internal_parser) <*> char_parser ']' 
+            <*> char_parser '(' <*> conditional_parser (fun chr -> chr != ')' && chr != '\n') <*> char_parser ')' in 
     any_of [bold_parser; italic_parser; link_parser; markdown_char_parser]
 and 
-get_parser p handler= pure handler <*> p <*> (parse_on_condition_lazy p internal_parser) <*> p 
-and
-get_parser2  p handler= pure handler <*> p <*> (parse_on_condition_lazy p internal_parser) <*> p 
+get_parser p handler= pure handler <*> p <*> (parse_on_condition_lazy (any_of [word_parser "\n\n"; p]) internal_parser) <*> p 
 
 let heading_parser =  
-  let handler tags text = Heading {level=(String.length tags); text=(List.fold_left (fun str chr -> str ^ (String.make 1 chr)) "" text)} in 
-  pure handler <*> any_of (List.map word_parser ["######";"#####";"####";"###";"##";"#"]) <*> conditional_parser (fun input -> input != '\n') 
+  let handler tags text = Heading {level=(String.length tags); components= text} in 
+  pure handler <*> any_of (List.map word_parser ["######";"#####";"####";"###";"##";"#"]) <*> 
+  one_or_more (internal_parser ())
+
+
+(*
+What's left? List, Paragraph (Outer level), continous parsing.   
+
+*)
