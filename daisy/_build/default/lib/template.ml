bdc761eb6f04b3af9e27b6c7e7a93e21963c79e5
Shafin Siddique <siddiqueshafin20@gmail.com>
2023-07-18 00:21:14 -0400

    fix infinite recursion bug.

--- daisy/_build/default/lib/template.ml
+++ daisy/_build/default/lib/template.ml
@@ -8,11 +8,13 @@ let boolean_parser =
       if value = "true" then ParsingSuccess (TrueExpr, rest) else ParsingSuccess (FalseExpr, rest)
     | ParsingError e -> ParsingError e)
 
-
+    
+let lazy_parser p = Parser (fun input -> run_parser (p ()) input)
 
 let rec template_expression_parser () = 
-  let with_brackets p = (pure (fun _ e _ -> e) <*> char_parser '(' <*> p () <*> char_parser ')') in 
-    pure (fun _ n _ -> n) <*> space_and_newline_parser <*> any_of [with_brackets template_expression_parser; boolean_parser] <*>  space_and_newline_parser
+  (* let with_brackets p =  in  *)
+    pure (fun _ n _ -> n) <*> space_and_newline_parser <*> any_of [lazy_parser template_with_brackets; boolean_parser] <*>  space_and_newline_parser
+and template_with_brackets () = (pure (fun _ e _ -> e) <*> char_parser '(' <*> template_expression_parser () <*> char_parser ')')
 
 let template_parser () = 
     pure (fun _ n _ -> n ) <*> word_parser "((" <*> template_expression_parser () <*> word_parser "))"
