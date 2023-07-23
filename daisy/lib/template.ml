efdc87cb6b0561177747b54e934a83ac089b7dbb
Shafin Siddique <siddiqueshafin20@gmail.com>
2023-07-22 20:32:32 -0400

    fixed if parser bug.

--- daisy/lib/template.ml
+++ daisy/lib/template.ml
@@ -37,6 +37,7 @@ let local_variable_parser =
     ParsingSuccess (str, rest) -> ParsingSuccess (LocalVariable str, rest)
     | ParsingError e -> ParsingError e)
   
+let chr_to_string c = String.make 1 c
 let site_variable_parser = pure (fun _ name ->  SiteVariable name)  <*> word_parser ".Site." <*> _local_variable_name_parser 
 
 let page_variable_parser = pure (fun _ name -> PageVariable name) <*> word_parser ".Page." <*> _local_variable_name_parser
@@ -51,14 +52,28 @@ and template_with_brackets () = (pure (fun _ e _ -> e) <*> char_parser '(' <*> t
 and if_block_parser () = pure (fun _ _ _ _ exprs _ _ -> exprs ) <*> space_and_newline_parser <*>  word_parser "((" <*> space_and_newline_parser <*> single_word_parser "if"
   <*> lazy_parser template_expression_parser <*> word_parser "))" <*> space_and_newline_parser 
 
-let endif_block_parser = pure (fun _ _ _ _ _-> "") <*> word_parser "((" <*> space_and_newline_parser <*> single_word_parser "endif" <*> space_and_newline_parser <*> word_parser "))"
+let endif_block_parser = pure (fun _ _ _ _ _-> "") <*> word_parser "((" <*> space_and_newline_parser <*> word_parser "endif" <*> space_and_newline_parser <*> word_parser "))"
 
-let template_char_parser = pure (fun c -> TemplateString (String.make 1 c)) <*>  any_parser
+let template_char_parser = pure (fun c spaces -> TemplateString ((String.make 1 c) ^ List.fold_left (fun str c -> str ^ (chr_to_string c)) "" spaces)) <*> any_parser <*> space_and_newline_parser
 
+let collapse_char str output = 
+  match output with 
+    [] -> [TemplateString str]
+    | (x::xs) -> match x with 
+      TemplateString existing -> List.cons (TemplateString (existing ^ str)) xs 
+      | _ -> List.cons (TemplateString str) output
+  
+let rec collapse lst output =
+  match lst with 
+    [] -> List.rev output 
+    | (x::xs) -> match x with 
+      TemplateString str -> collapse xs (collapse_char str output)  
+      | _ -> collapse xs (List.cons x output)
 let rec template_parser () = 
     let main_parser = pure (fun _ n _ -> n )  <*>  word_parser "((" <*> template_expression_parser () <*> word_parser "))" in 
     pure (fun _ p _ -> p) <*> space_and_newline_parser <*> any_of [main_parser; lazy_parser if_parser] <*> space_and_newline_parser
 
-and if_parser () = pure (fun exprs body _  -> IfExpr (exprs, body)) <*> lazy_parser if_block_parser <*> parse_on_condition endif_block_parser
+
+and if_parser () = pure (fun exprs body _  -> IfExpr (exprs, collapse body [])) <*> lazy_parser if_block_parser <*> parse_on_condition endif_block_parser
   (any_of [lazy_parser template_parser; template_char_parser]) <*> endif_block_parser
 
